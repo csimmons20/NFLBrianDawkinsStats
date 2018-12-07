0398c33b75ffc131ba52e5705f9286f521249949
csimmons20 <csimmons20@scienceleadership.org>
2018-12-07 13:46:42 -0500

    Working on Model & Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -20,11 +20,11 @@ public class Controller {
 
 public ImageView Player;
 public ImageView TeamLogo;
-public ChoiceBox CBY;
-public Label TextViewFF;
-public Label TextViewCOMB;
-public Label TextViewINT;
-public Label TextViewSack;
+public ChoiceBox UserSelects;
+public Label ffEdit;
+public Label combEdit;
+public Label intEdit;
+public Label sackEdit;
 
  Model ourModel;
 
@@ -33,66 +33,66 @@ public Label TextViewSack;
 
 
 
-        CBY.setValue(ourModel.getCBchoice());
-        CBY.getItems().addAll("1996", "1997", "It's 1998", "1999", "2000", "2001", "2002", "2003",
+        UserSelects.setValue(ourModel.getCBchoice());
+        UserSelects.getItems().addAll("1996", "1997", "It's 1998", "1999", "2000", "2001", "2002", "2003",
                 "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011");
-       CBY.setOnAction(e-> {
+       UserSelects.setOnAction(e-> {
                    System.out.println("choice box action happened");
 
-                   if (CBY.valueProperty().get().equals("1996")) {
+                   if (UserSelects.valueProperty().get().equals("1996")) {
                        System.out.println("1996 action happened");
                    }
-           if (CBY.valueProperty().get().equals("1997")) {
+           if (UserSelects.valueProperty().get().equals("1997")) {
                System.out.println("1997 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("1998")) {
+           if (UserSelects.valueProperty().get().equals("1998")) {
                System.out.println("1998 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("1999")) {
+           if (UserSelects.valueProperty().get().equals("1999")) {
                System.out.println("1999 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2000")) {
+           if (UserSelects.valueProperty().get().equals("2000")) {
                System.out.println("2000 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2001")) {
+           if (UserSelects.valueProperty().get().equals("2001")) {
                System.out.println("2001 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2002")) {
+           if (UserSelects.valueProperty().get().equals("2002")) {
                System.out.println("2002 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2003")) {
+           if (UserSelects.valueProperty().get().equals("2003")) {
                System.out.println("2003 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2004")) {
+           if (UserSelects.valueProperty().get().equals("2004")) {
                System.out.println("2004 action happened");
 
            }
-           if (CBY.valueProperty().get().equals("2005")) {
+           if (UserSelects.valueProperty().get().equals("2005")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2006")) {
+           }if (UserSelects.valueProperty().get().equals("2006")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2007")) {
+           }if (UserSelects.valueProperty().get().equals("2007")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2008")) {
+           }if (UserSelects.valueProperty().get().equals("2008")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2009")) {
+           }if (UserSelects.valueProperty().get().equals("2009")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2010")) {
+           }if (UserSelects.valueProperty().get().equals("2010")) {
                System.out.println("1996 action happened");
 
-           }if (CBY.valueProperty().get().equals("2011")) {
+           }if (UserSelects.valueProperty().get().equals("2011")) {
                System.out.println("1996 action happened");
 
            }
