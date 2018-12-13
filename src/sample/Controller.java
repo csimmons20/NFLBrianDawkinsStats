959d4265610a50f5c75702219fffb888efecdf4c
csimmons20 <csimmons20@scienceleadership.org>
2018-12-13 09:58:19 -0500

    Working on Model & Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -58,7 +58,11 @@ public Label sackEdit;
 
                        System.out.println("1996 action happened");
                        TeamLogo.setImage(philly);
-
+                       ourModel.setYear(1996);
+                       combEdit.setText(ourModel.descriptionCOMB());
+                       ffEdit.setText(ourModel.descriptionFF());
+                       sackEdit.setText(ourModel.descriptionSack());
+                       intEdit.setText(ourModel.descriptionINT());
 
                    }
            if (UserSelects.valueProperty().get().equals("1997")) {
@@ -67,77 +71,164 @@ public Label sackEdit;
                TeamLogo.setImage(philly);
                ourModel.setYear(1997);
                combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
 
            }
            if (UserSelects.valueProperty().get().equals("1998")) {
 
                System.out.println("1998 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(1998);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("1999")) {
 
                System.out.println("1999 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(1999);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2000")) {
 
                System.out.println("2000 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2000);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2001")) {
 
                System.out.println("2001 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2001);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2002")) {
 
                System.out.println("2002 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2002);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2003")) {
 
                System.out.println("2003 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2003);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2004")) {
 
                System.out.println("2004 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2004);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2005")) {
 
                System.out.println("2005 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2005);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2006")) {
 
                System.out.println("2006 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2006);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2007")) {
 
                System.out.println("2007 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2007);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2008")) {
 
                System.out.println("2008 action happened");
                TeamLogo.setImage(philly);
+               ourModel.setYear(2008);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2009")) {
 
                System.out.println("2009 action happened");
                TeamLogo.setImage(denver);
+               ourModel.setYear(2009);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2010")) {
 
                System.out.println("2010 action happened");
                TeamLogo.setImage(denver);
+               ourModel.setYear(2010);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
            if (UserSelects.valueProperty().get().equals("2011")) {
 
                System.out.println("2011 action happened");
                TeamLogo.setImage(denver);
+               ourModel.setYear(2011);
+               combEdit.setText(ourModel.descriptionCOMB());
+               ffEdit.setText(ourModel.descriptionFF());
+               sackEdit.setText(ourModel.descriptionSack());
+               intEdit.setText(ourModel.descriptionINT());
+
            }
                }
 
